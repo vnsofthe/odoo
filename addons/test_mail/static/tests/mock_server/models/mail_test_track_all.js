347f5aea7ec387cc945ee5dc99925e0b9ae64fcd
vnsoft <vnsoft.he@gmail.com>
2025-05-26 08:59:22 +0800

    [odoog] delete files

--- addons/test_mail/static/tests/mock_server/models/mail_test_track_all.js
+++
@@ -1,6 +0,0 @@
-import { models } from "@web/../tests/web_test_helpers";
-
-export class MailTestTrackAll extends models.ServerModel {
-    _name = "mail.test.track.all";
-    _inherit = ["mail.thread"];
-}
