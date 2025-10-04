23108ef5333be90ae509dd10efe9369fd4f0b159
vnsoft <vnsoft.he@gmail.com>
2025-10-04 12:35:06 +0800

    [odoog]delete files

--- addons/l10n_jo_edi_extended/wizard/account_move_send.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class AccountMoveSend(models.TransientModel):
-    _inherit = 'account.move.send'
-
-    l10n_jo_edi_demo_mode = fields.Boolean(related='company_id.l10n_jo_edi_demo_mode')
